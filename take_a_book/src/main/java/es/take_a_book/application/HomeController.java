45ab43c4b13fc0348fdce3b344aef596ac44d1b2
ElDiegu <34305565+ElDiegu@users.noreply.github.com>
2023-02-08 20:12:20 +0100

    First commit
    First commit with a functional empty template and controller.

---
+++ take_a_book/src/main/java/es/take_a_book/application/HomeController.java
@@ -0,0 +1,14 @@
+package es.take_a_book.application;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	@GetMapping("/home")
+	public String home(Model model) {
+		model.addAttribute("name", "Mundo");
+		return "home";
+	}
+}
---
+++ take_a_book/src/main/java/es/take_a_book/application/TakeABookApplication.java
@@ -0,0 +1,13 @@
+package es.take_a_book.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakeABookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakeABookApplication.class, args);
+	}
+
+}
