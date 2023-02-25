299e6a5505285d5e3af1ac8cc3223c1ffc434cc8
ElDiegu <34305565+ElDiegu@users.noreply.github.com>
2023-02-25 11:36:29 +0100

    Author services
    Author services, controller and repository updates.
    Authors html page

--- take_a_book/src/main/java/es/take_a_book/application/repository/UserRepository.java
+++ take_a_book/src/main/java/es/take_a_book/application/repository/UserRepository.java
@@ -3,7 +3,6 @@ package es.take_a_book.application.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import es.take_a_book.application.model.Users;
 
-
 public interface UserRepository extends JpaRepository<Users, String> {
 	
 }
