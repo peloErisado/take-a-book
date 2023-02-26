c8803ecaaa88eecda1e6a3210125d7b6ade40384
ElDiegu <eldieguuu@gmail.com>
2023-02-26 21:50:49 +0100

    Author service complete
    Author creation, update, and removal implemented.

--- take_a_book/src/main/java/es/take_a_book/application/controller/AuthorController.java
+++ take_a_book/src/main/java/es/take_a_book/application/controller/AuthorController.java
@@ -18,7 +18,6 @@ import es.take_a_book.application.model.Author;
 import es.take_a_book.application.model.Book;
 import es.take_a_book.application.service.AuthorService;
 import es.take_a_book.application.service.BookService;
-
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 
@@ -32,11 +31,13 @@ public class AuthorController {
 	@Autowired
 	private BookService bookService;
 	
+	private String path = "author_HTML/";
+	
 	//Authors
 	@GetMapping("")
 	public String getAuthors(Model model) {
 		model.addAttribute("authors", authorService.findAll());
-		return "show_authors";
+		return path+"author_show_multiple";
 	}
 	
 	@GetMapping("/{id}")
@@ -47,47 +48,68 @@ public class AuthorController {
 		if (author.isPresent()) {
 			model.addAttribute("author", author.get());
 			model.addAttribute("books", author.get().getBooks());
-			return "show_author";
+			return path+"author_show_single";
 		}else {
-			return "show_authors";
+			return path+"author_show_multiple";
 		}
+	}
+	
+	@GetMapping("/new")
+	public String newAuthorPage() {
+		return path+"author_add";
+	}
+	
+	@PostMapping("/new")
+	public String newAuthor(Model model, String name, String surnames, String description) throws IOException{
+		authorService.save(new Author(name, surnames, description));
+		model.addAttribute("authors", authorService.findAll());
+		return path+"author_show_multiple";
 	}
 	
 	@GetMapping("/{id}/edit")
 	public String editAuthor(Model model, @PathVariable Long id) {
 		model.addAttribute("author", authorService.findById(id).get());
-		return "edit_author";
+		return path+"author_edit";
 	}
 	
-	@PostMapping("/new")
-	public String newAuthor(Model model, @PathVariable String name, 
-				@PathVariable String surnames, @PathVariable String description) throws IOException{
-		authorService.save(new Author(name, surnames, description));
-		return "show_authors";
-	}
-	
-	@PostMapping("/{id}/edited")
-	public String editedAuthor(Model model, @PathVariable Long id, @PathVariable Optional<String> name,
-				@PathVariable Optional<String> surnames, @PathVariable Optional<String> description,
-				@PathVariable Optional<Integer> book_ISBN) throws IOException{
+	@PostMapping("/{id}/edit")
+	public String editedAuthor(Model model, @PathVariable Long id, String name, String surnames, String description, Integer book_ISBN) throws IOException{
 		
 		model.addAttribute("authors", authorService.findAll());
 		
 		Optional<Author> author = authorService.findById(id);
 		
-		if(!author.isPresent()) return "show_authors";
+		if(!author.isPresent()) return "author_HTML/show_authors";
 		
-		if(!name.isEmpty()) author.get().setName(name.get());
+		author.get().setName(name);
 		
-		if(!surnames.isEmpty()) author.get().setSurnames(surnames.get());
+		author.get().setSurnames(surnames);
 		
-		if(!description.isEmpty()) author.get().setDescription(description.get());
+		author.get().setDescription(description);
 		
-		if(!book_ISBN.isEmpty()) {
-			Optional<Book> book = bookService.findById(book_ISBN.get());
+		if(book_ISBN > 0) {
+			Optional<Book> book = bookService.findById(book_ISBN);
 			if(book.isPresent()) authorService.addBook(id, book.get());
 		}
 		
-		return "show_authors";
+		authorService.save(author.get());
+		
+		return path+"author_show_multiple";
+	}
+	
+	@GetMapping("/{id}/remove_confirm")
+	public String deleteAuthorPage(Model model, @PathVariable Long id) {
+		model.addAttribute("author", authorService.findById(id).get());
+		return path+"author_remove";
+	}
+	
+	@GetMapping("/{id}/remove")
+	public String deleteAuthor(Model model, @PathVariable Long id) {
+		Optional<Author> author = authorService.findById(id);
+		if(author.isPresent()) authorService.delete(author.get());
+		
+		model.addAttribute("authors", authorService.findAll());
+		
+		return path+"author_show_multiple";
 	}
 }
