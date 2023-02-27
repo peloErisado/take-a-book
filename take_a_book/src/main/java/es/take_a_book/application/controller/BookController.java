72a44983785e782e2fe1f9ea55ff00e6a3d2935c
ElDiegu <eldieguuu@gmail.com>
2023-02-27 18:19:54 +0100

    Images POST and GET
    POST and GET of image files through database column, images can be uploaded using Postman.

--- take_a_book/src/main/java/es/take_a_book/application/controller/BookController.java
+++ take_a_book/src/main/java/es/take_a_book/application/controller/BookController.java
@@ -14,10 +14,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
 import es.take_a_book.application.model.Book;
 import es.take_a_book.application.service.BookService;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 @Controller
 @RequestMapping("/books")
 public class BookController {
@@ -25,13 +28,7 @@ public class BookController {
 	//private static final String BOOKS_FOLDER = "books";
 	@Autowired
 	private BookService bookService;
-	
-	@PostConstruct
-	void init() {
-		bookService.save(new Book(9293, "Titulo1", "Genero1", "Lengua1", "Publisher1", "Sinopsis1", (float)18.90, 1923));
-		bookService.save(new Book(9294, "Titulo2", "Genero2", "Lengua2", "Publisher2", "Sinopsis2", (float)19.90, 1924));
-	}
-	
+
 	//REDIRECTS: To adding books page
 	@GetMapping("/new")
 	public String showAddBookScreen() {
@@ -72,21 +69,44 @@ public class BookController {
 	}
 
 	//DOWNLOADS: A book's image
+	
 	@GetMapping("/{ISBN}/image")
 	public ResponseEntity<Object> downloadImage(@PathVariable int ISBN) throws SQLException {
 
 		Optional <Book> book = bookService.findById(ISBN);
-
-		if (book.isPresent() && book.get().getImageFile() != null) {
-
-			Resource file = new InputStreamResource(book.get().getImageFile().getBinaryStream());
-
-			return ResponseEntity.ok().header(HttpHeaders.CONTENT_TYPE, "image/jpeg")
-					.contentLength(book.get().getImageFile().length()).body(file);
-
-		} else {
-			return ResponseEntity.notFound().build();
-		}
+		
+		if(book.isEmpty() || book.get().getImageFile() == null) return ResponseEntity.notFound().build();
+		
+		Resource file = new InputStreamResource (book.get().getImageFile().getBinaryStream());
+		
+		return ResponseEntity.ok().header(HttpHeaders.CONTENT_TYPE, "image/jpg")
+				.contentLength(book.get().getImageFile().length()).body(file);
+	}
+	
+	@PostMapping("/{ISBN}/image")
+	public ResponseEntity<Object> uploadImage (Model model, @PathVariable Integer ISBN, 
+										@RequestBody MultipartFile image) throws IOException {
+		
+		Optional<Book> book = bookService.findById(ISBN);
+		if(book.isEmpty()) return ResponseEntity.notFound().build();
+		/*=======================================*/
+		book.get().setImageFile(BlobProxy.generateProxy(image.getInputStream(), image.getSize()));
+		System.out.println("\n"+image.getSize());
+		bookService.save(book.get());
+		/*=======================================*/
+		return ResponseEntity.ok().build();
+	}
+	
+	@DeleteMapping("/{ISBN}/image")
+	public ResponseEntity<Object> deleteImage (Model model, @PathVariable Integer ISBN) {
+		
+		Optional<Book> book = bookService.findById(ISBN);
+		if(book.isEmpty() || book.get().getImageFile() == null) return ResponseEntity.notFound().build();
+		
+		book.get().setImageFile(null);
+		bookService.save(book.get());
+		
+		return ResponseEntity.ok().build();
 	}
 /*
 	//UPLOADS: A book's image
@@ -122,9 +142,5 @@ public class BookController {
 		 }
 	}
 	*/
-
-	
-	
-	
 	
 }
